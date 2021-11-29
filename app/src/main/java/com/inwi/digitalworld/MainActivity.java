0afbe0f073c42ff2460ad080f97ef7a2946371e9
JuliMontes <jamontesg@uqvirtual.edu.co>
2021-11-29 15:41:15 -0500

    Se añade barra de progreso y se ajusta el cambio de apariencia

--- app/src/main/java/com/inwi/digitalworld/MainActivity.java
+++ app/src/main/java/com/inwi/digitalworld/MainActivity.java
@@ -3,17 +3,26 @@ package com.inwi.digitalworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar progress_bar_splash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progress_bar_splash = findViewById(R.id.progress_bar_splash);
 
         TimerTask task = new TimerTask() {
             @Override
@@ -24,8 +33,42 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+
         Timer time = new Timer();
-        time.schedule(task, 5000);
+        time.schedule(task, 3000);
+
 
     }
+
+    public class ShowCustomProgressBarAsyncTask extends AsyncTask<Void, Integer, Void> {
+        int myProgress;
+
+        @Override
+        protected void onPostExecute(Void result) {
+            progress_bar_splash.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            myProgress = 0;
+            progress_bar_splash.setSecondaryProgress(0);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while (myProgress < 100) {
+                myProgress++;
+                publishProgress(myProgress);
+                SystemClock.sleep(100);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progress_bar_splash.setProgress(values[0]);
+            progress_bar_splash.setSecondaryProgress(values[0] + 5);
+        }
+    }
+
 }
