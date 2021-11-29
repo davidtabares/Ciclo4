d6018d2011b23dcb1898ae48f3f09322ca52aa61
JuliMontes <jamontesg@uqvirtual.edu.co>
2021-11-29 15:21:02 -0500

    Prueba splash screen

---
+++ app/src/main/java/com/inwi/digitalworld/MainActivity.java
@@ -0,0 +1,31 @@
+package com.inwi.digitalworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer time = new Timer();
+        time.schedule(task, 5000);
+
+    }
+}
