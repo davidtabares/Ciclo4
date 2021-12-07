94364ce0205412c613b0c21f73d8843f0731aaff
JuliMontes <jamontesg@uqvirtual.edu.co>
2021-12-07 10:29:01 -0500

    Se implementa personalización del nav_header_menu, permitiendo modificar la imagen.

--- app/src/main/java/com/inwi/digitalworld/LoginActivity.java
+++ app/src/main/java/com/inwi/digitalworld/LoginActivity.java
@@ -1,9 +1,7 @@
 package com.inwi.digitalworld;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -16,7 +14,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.inwi.digitalworld.database.UserDatabase;
 import com.inwi.digitalworld.database.model.User;
 import com.inwi.digitalworld.util.Constant;
@@ -29,74 +26,56 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.lang.ref.WeakReference;
 import java.util.List;
-
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-
     private Button btn_login, btn_sign_up;
-
     private EditText edt_user, edt_password;
-
     private final int ACTIVITY_REGISTER = 1;
-
     private SharedPreferences myPreferences;
-
     private UserDatabase database;
-
     private List<User> listUsers;
-
     private FirebaseAuth myAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         btn_login = findViewById(R.id.btn_login);
         btn_sign_up = findViewById(R.id.btn_sign_up);
-
         edt_user = findViewById(R.id.edt_user);
         edt_password = findViewById(R.id.edt_password);
-
         btn_login.setOnClickListener(this);
         btn_sign_up.setOnClickListener(this);
-
         myPreferences = getSharedPreferences(Constant.PREFERENCE, MODE_PRIVATE);
-
         database = UserDatabase.getInstance(this);
 
         String user = myPreferences.getString("user", "");
         String password = myPreferences.getString("password", "");
+        String firstName = myPreferences.getString("firstName", "");
+        String image = myPreferences.getString("image", "");
 
         if (!user.equals("") && !password.equals("")) {
-            toLogin(user, password);
+            toLogin(user, password, firstName, image);
         }
 
         new GetUserTask(this).execute();
-
         myAuth = FirebaseAuth.getInstance();
-
     }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_login:
                 String user = edt_user.getText().toString();
                 String password = edt_password.getText().toString();
-
                 Log.e("USER", user);
                 Log.e("PASSWORD", password);
 
                 if (user.equals("admin@admin.com") && password.equals("admin")) {
 
-                    toLogin(user, password);
+                    toLogin(user, password, "Admin", "");
 
                 }
                 else {
-
                     loginFirestore(user, Utilities.md5(password));
                     //loginFirebase(user, password);
                     //new GetUserLoginTask(this, user, Utilities.md5(password));
@@ -109,9 +88,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 break;
         }
     }
-
     public void loginFirestore(String email, String password) {
-
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference docRef = db.collection("users").document(email);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -123,14 +100,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         Log.e("TAG", "DocumentSnapshot data: " + document.getData());
 
                         String userPassword = document.getData().get("password").toString();
+                        String userFirstName = document.getData().get("firstName").toString();
+                        String userImage = document.getData().get("image").toString();
 
                         if (password.equals(userPassword)) {
-                            toLogin(email, password);
+                            toLogin(email, password, userFirstName, userImage);
                         } else {
                             Toast.makeText(LoginActivity.this, getResources().getString(R.string.txt_password_error), Toast.LENGTH_SHORT).show();
                         }
-
-
                     } else {
                         Log.e("TAG", "No such document");
                         Toast.makeText(LoginActivity.this, getResources().getString(R.string.txt_login_wrong), Toast.LENGTH_SHORT).show();
@@ -140,9 +117,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 }
             }
         });
-
     }
-
     public void loginFirebase(String user, String password) {
         myAuth.signInWithEmailAndPassword(user, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -152,7 +127,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             // Sign in success, update UI with the signed-in user's information
                             Log.e("TAG", "signInWithEmail:success");
                             FirebaseUser userFB = myAuth.getCurrentUser();
-                            toLogin(user, password);
+                            toLogin(user, password, "", "");
                         }
                         else {
                             // If sign in fails, display a message to the user.
@@ -162,12 +137,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 });
     }
 
-    public void toLogin(String user, String password){
+    public void toLogin(String user, String password, String firstName, String image){
         Toast.makeText(this, getResources().getString(R.string.txt_logged), Toast.LENGTH_SHORT).show();
 
         SharedPreferences.Editor editor = myPreferences.edit();
         editor.putString("user", user);
         editor.putString("password", password);
+        editor.putString("firstName", firstName);
+        editor.putString("image", image);
 
         editor.commit();
 
@@ -176,34 +153,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         intent.putExtra("password", password);
         startActivity(intent);
         finish();
-
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if(requestCode == ACTIVITY_REGISTER){
-
             if (resultCode == Activity.RESULT_OK) {
-
                 String user = data.getStringExtra("email");
                 String password = data.getStringExtra("password");
 
-                toLogin(user, password);
+                toLogin(user, password, "", "");
 
             }
         }
     }
-
     private static class GetUserTask extends AsyncTask<Void, Void, List<User>> {
-
         private WeakReference<LoginActivity> loginActivityWeakReference;
-
         GetUserTask(LoginActivity context) {
             this.loginActivityWeakReference = new WeakReference<>(context);
         }
-
         @Override
         protected List<User> doInBackground(Void... voids) {
             if (loginActivityWeakReference.get() != null) {
@@ -212,7 +180,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
             return null;
         }
-
         @Override
         protected void onPostExecute(List<User> users) {
             if (users != null && users.size() > 0) {
@@ -220,22 +187,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
             super.onPostExecute(users);
         }
-
     }
-
     private static class GetUserLoginTask extends AsyncTask<Void, Void, User> {
-
         private String email;
         private String password;
         private WeakReference<LoginActivity> loginActivityWeakReference;
-
         GetUserLoginTask(LoginActivity context, String email, String password) {
             this.loginActivityWeakReference = new WeakReference<>(context);
             this.email = email;
             this.password = password;
             doInBackground();
         }
-
         @Override
         protected User doInBackground(Void... voids) {
             if (loginActivityWeakReference.get() != null) {
@@ -245,12 +207,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
             return null;
         }
-
         @Override
         protected void onPostExecute(User user) {
             if (user != null) {
 
-                loginActivityWeakReference.get().toLogin(email, password);
+                loginActivityWeakReference.get().toLogin(email, password, "", "");
             }
             else {
                 Log.e("LOGINTASK", "LOGIN ERROR");
@@ -258,6 +219,4 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             super.onPostExecute(user);
         }
     }
-
-
 }
--- app/src/main/java/com/inwi/digitalworld/MenuActivity.java
+++ app/src/main/java/com/inwi/digitalworld/MenuActivity.java
@@ -2,21 +2,39 @@ package com.inwi.digitalworld;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.inwi.digitalworld.util.Constant;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
@@ -27,37 +45,43 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MenuActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private SharedPreferences myPreferences;
     private Activity myActivity;
+    final int OPEN_GALLERY = 1;
+
+    Uri data1;
+    String urlImage;
+
+    ImageView navImage;
+    String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_menu);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         myActivity = this;
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Snackbar.make(view, getResources().getString(R.string.txt_add_product), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
                 //El fab abre fragments desde el menú
                 Navigation.findNavController(myActivity, R.id.nav_host_fragment_content_menu).navigate(R.id.nav_add_product);
-
             }
         });
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -69,26 +93,27 @@ public class MenuActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id = menuItem.getItemId();
                 //it's possible to do more actions on several items, if there is a large amount of items I prefer switch(){case} instead of if()
                 if (id == R.id.nav_logout){
-
                     SharedPreferences.Editor editor = myPreferences.edit();
 
                     editor.putString("user", "");
                     editor.putString("password", "");
+                    editor.putString("firstName", "");
+                    editor.putString("image", "");
                     editor.commit();
+
+                    //editor.clear();
 
                     Intent intent = new Intent(myActivity, LoginActivity.class);
                     startActivity(intent);
 
                     finish();
                     return true;
-
                 }
                 //This is for maintaining the behavior of the Navigation view
                 NavigationUI.onNavDestinationSelected(menuItem,navController);
@@ -97,9 +122,7 @@ public class MenuActivity extends AppCompatActivity {
                 return true;
             }
         });
-
         Bundle bundle = getIntent().getExtras();
-
         if(bundle != null) {
             Log.e("BUN_USER", bundle.getString("user"));
             Log.e("BUN_PASSWORD", bundle.getString("password"));
@@ -107,31 +130,156 @@ public class MenuActivity extends AppCompatActivity {
 
         myPreferences = getSharedPreferences(Constant.PREFERENCE, MODE_PRIVATE);
 
-        String user = myPreferences.getString("user", getResources().getString(R.string.txt_no_user));
+        user = myPreferences.getString("user", getResources().getString(R.string.txt_no_user));
         String password = myPreferences.getString("password", getResources().getString(R.string.txt_no_password));
+        String firstName = myPreferences.getString("firstName", "No Name");
+        String image = myPreferences.getString("image", "");
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView navName = headerView.findViewById(R.id.tev_header_name);
+        TextView navEmail= headerView.findViewById(R.id.tev_header_email);
+
+        navImage = headerView.findViewById(R.id.imv_header_image);
+        navImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, getResources().getString(R.string.txt_product_select)), OPEN_GALLERY);
+
+            }
+        });
+
+        Glide.with(myActivity)
+                .load(image)
+                .placeholder(new ColorDrawable(Color.BLACK))
+                .into(navImage);
+
+        navName.setText(firstName);
+        navEmail.setText(user);
 
         if (user.equals("admin@admin.com") || user.equals("andiga22@hotmail.com")) {
             fab.setVisibility(View.VISIBLE);
         }
-
 /*        if (user.contains("@digitalworld.com")) {
             fab.setVisibility(View.VISIBLE);
         }*/
-
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
-
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == OPEN_GALLERY) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                data1 = data.getData();
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data1);
+                    navImage.setImageBitmap(bitmap);
+
+                    uploadImage();
+
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else {
+                Toast.makeText(myActivity, getResources().getString(R.string.txt_product_image_error), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    public void uploadImage() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference();
+        //if there is a file to upload
+        if (data1 != null) {
+            //displaying a progress dialog while upload is going on
+
+            Calendar c = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+            String strDate = sdf.format(c.getTime());
+            String nameImage = strDate + ".jpg";
+
+            myPreferences = getSharedPreferences(Constant.PREFERENCE, MODE_PRIVATE);
+
+            String user = myPreferences.getString("user", "NO USER");
+
+
+            StorageReference riversRef = storageReference.child(user + "/" + nameImage);
+            riversRef.putFile(data1)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //if the upload is successfull
+                            //hiding the progress dialog
+
+                            //and displaying a success toast
+
+                            riversRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    urlImage = uri.toString();
+                                    Log.e("URL_IMAGE", urlImage);
+
+                                    SharedPreferences.Editor editor = myPreferences.edit();
+                                    editor.putString("image", urlImage);
+                                    editor.commit();
+
+                                    Map<String, Object> data = new HashMap<>();
+                                    data.put("image", urlImage);
+
+                                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                    db.collection("users").document(user)
+                                            .set(data, SetOptions.merge());
+
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle any errors
+                                }
+                            });
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            //if the upload is not successfull
+                            //hiding the progress dialog
+
+
+                            //and displaying error message
+                            Toast.makeText(myActivity, exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+        //if there is not any file
+        else {
+            //you can display an error toast
+        }
+    }
+
 
 }
